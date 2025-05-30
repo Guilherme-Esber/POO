6ae84b3b744455543d851e89adb90277b8653969
Guilherme Esber <D2024005516@unifei.edu.br>
2025-05-30 16:44:44 -0300

    Update LAB 7 - Ex politico.cpp

--- LAB 7 - Ex politico.cpp
+++ LAB 7 - Ex politico.cpp
@@ -1,22 +1,42 @@
-#include "politico.h"
+#include "Politicos.h"
 
-void Politico::imprime() {
-    cout << "Numero: " << numero << endl;
-    cout << "Nome: " << nome << endl;
-    cout << "Partido: " << partido << endl;
+Politico::Politico(const string& nome) : nome(nome) {
+    cout << "Entrando no construtor da classe Politico" << endl;
 }
 
-void Presidente::imprime() {
-    Politico::imprime();
-    cout << "País: " << pais << endl;
+void Politico::Imprime() const {
+    cout << "Politico: " << nome << endl;
 }
 
-void Governador::imprime() {
-    Presidente::imprime();
-    cout << "Estado: " << estado << endl;
+
+Presidente::Presidente(const string& nome, int mandato)
+    : Politico(nome), mandato(mandato) {
+    cout << "Entrando no construtor da classe Presidente" << endl;
 }
 
-void Prefeito::imprime() {
-    Governador::imprime();
-    cout << "Cidade: " << cidade << endl << endl;
+void Presidente::Imprime() const {
+    Politico::Imprime();
+    cout << "Cargo: Presidente, Mandato: " << mandato << " anos" << endl;
+}
+
+
+Governador::Governador(const string& nome, const string& estado)
+    : Politico(nome), estado(estado) {
+    cout << "Entrando no construtor da classe Governador" << endl;
+}
+
+void Governador::Imprime() const {
+    Politico::Imprime();
+    cout << "Cargo: Governador do estado de " << estado << endl;
+}
+
+
+Prefeito::Prefeito(const string& nome, const string& cidade)
+    : Politico(nome), cidade(cidade) {
+    cout << "Entrando no construtor da classe Prefeito" << endl;
+}
+
+void Prefeito::Imprime() const {
+    Politico::Imprime();
+    cout << "Cargo: Prefeito da cidade de " << cidade << endl;
 }
