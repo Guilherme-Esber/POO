842b9f2d84e236aba9cac5635f404bdc211fa6d6
Guilherme Esber <D2024005516@unifei.edu.br>
2025-06-09 20:43:03 -0300

    Create lab8ex1.h

---
+++ lab8ex1.h
@@ -0,0 +1,47 @@
+#ifndef POLITICOS_H
+#define POLITICOS_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Politico {
+protected:
+    string nome;
+
+public:
+    Politico(const string& nome);
+    virtual ~Politico() = default;
+
+    virtual void Imprime() const;
+};
+
+class Presidente : public Politico {
+private:
+    int mandato;
+
+public:
+    Presidente(const string& nome, int mandato);
+    void Imprime() const override;
+};
+
+class Governador : public Politico {
+private:
+    string estado;
+
+public:
+    Governador(const string& nome, const string& estado);
+    void Imprime() const override;
+};
+
+class Prefeito : public Politico {
+private:
+    string cidade;
+
+public:
+    Prefeito(const string& nome, const string& cidade);
+    void Imprime() const override;
+};
+
+#endif
