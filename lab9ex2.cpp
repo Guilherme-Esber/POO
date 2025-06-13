ea601f60108e7a44a3dfe8e1b0e800413f21c54f
Guilherme Esber <D2024005516@unifei.edu.br>
2025-06-13 16:36:49 -0300

    Update lab9ex2.cpp

--- lab9ex2.cpp
+++ lab9ex2.cpp
@@ -5,13 +5,11 @@
 
 // Função para alocar continuamente vetores de double até que ocorra uma exceção
 void alocarMemoria() {
-    std::vector<double> *vetor = nullptr;
-
     while (true) {
         try {
             // Tenta alocar um vetor com 10 milhões de elementos
-            vetor = new std::vector<double>(10000000, 0.0);
-            std::cout << "Alocado um vetor de tamanho " << vetor->size() << std::endl;
+            std::vector<double> vetor(10000000, 0.0);
+            std::cout << "Alocado um vetor de tamanho " << vetor.size() << std::endl;
         }
         catch (const std::bad_alloc& e) {
             std::cerr << "Erro de alocação de memória: " << e.what() << std::endl;
